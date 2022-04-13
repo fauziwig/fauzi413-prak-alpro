39cb3791e7a437dc60c58d480dc6f42f5b7b274f
Fauzi Triagung Wiguna <101690263+fauziwig@users.noreply.github.com>
2022-04-13 10:41:15 +0700

    upload file kegiatan4-prakalpro

---
+++ kegiatan4-prakalpro.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+class Operator{
+	public:
+		long faktorial();
+		long faktorial(int);
+		
+	private:
+		int n;
+		long hasil;
+		
+};
+
+//iteratif/perulangan
+long Operator::faktorial(){
+	long fak=1;
+	for(int i=1; i<=n; i++){
+		fak = fak * 1;
+	return fak;
+	}
+}
+
+//rekursif
+long Operator::faktorial(int n){
+	if(n==0 || n==1){
+		return (1);
+	}
+	else {
+		return (n*faktorial(n-1));
+	}
+			
+}
+
+int main(){
+	int angka;
+	cin>>angka;
+	Operator jalan;
+	cout<<jalan.faktorial(angka);//pemanggilan fungsi dengan parameter
+	cout<<jalan.faktorial()<<endl;//pemanggilan fungsi tanpa parameter
+}
